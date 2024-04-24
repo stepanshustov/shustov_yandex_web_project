5b73d5ad11b3553382dfdad6a35f970cfd7deda0
shust <step.shustov@mail.ru>
2024-04-24 15:21:58 +0400

    commit 1

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "6729828743:AAGkJSNw40jct3LQ5lG96myP5DZBKq4Ge5k"
+weather_app_key = '54724f8408ddbe7abef8e3e1eb804968'
+weather_app_id = 'ba7447c3'
+maps_token = "40d1649f-0493-4b70-98ba-98533de7710b"
