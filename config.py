cce13e7e016ac1ce979b7379dcae2c686df4f0bd
stepan <step.shustov@mail.ru>
2025-04-08 20:59:47 +0400

    dfds

--- config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = "7525700549:AAH9b-uv4j3PqNHpyY4ZF0-zKeMaPWD8qTc"
-weather_app_key = '54724f8408ddbe7abef8e3e1eb804968'
-weather_app_id = 'ba7447c3'
-maps_token = "40d1649f-0493-4b70-98ba-98533de7710b"
